556c204bec3d06a93a124dba43bcf58c4295427c
Brian Backes <35049560+Backnard@users.noreply.github.com>
2020-05-26 23:09:30 -0700

    fixed typo

--- db/seed.js
+++ db/seed.js
@@ -68,7 +68,7 @@ async function buildInitialDb() {
     }
 }
    
-async function initializeusers() {
+async function initializeUsers() {
     try {
         console.log('starting to create users...');
 
@@ -86,7 +86,7 @@ try {
 
     await dropTables();
     await buildInitialDb();
-    await initializeusers();
+    await initializeUsers();
 
 } catch (error) {
 
