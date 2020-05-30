3a216cd283a56476717fae8175359ed1a36560c2
Sean Greene <seangreenebrandeis@gmail.com>
2020-05-30 15:38:04 -0400

    added more routines

--- api/routines.js
+++ api/routines.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routinesRouter = express.Router();
-const {getPublicRoutines, createRoutine} = require('../db');
+const {getPublicRoutines, createRoutine, updateRoutine} = require('../db');
 const {requireUser} = require('./utils');
 
 routinesRouter.get('/', async (req, res, next) => {
@@ -34,6 +34,53 @@ routinesRouter.post('/', requireUser, async (req, res, next) => {
     } catch (error) {
         next(error);
     }
+})
+
+routinesRouter.patch('/:routinesId',requireUser, async (req, res, next)=>{
+
+    const body = { id, creatorId, publica, name, goal } = req.body;
+
+    console.log('Entered /routineId PATCH, req body: ', body);
+
+    if(requireUser.user.id === creatorId){
+        console.log("permission to edit routine granted");
+        const { routinesId } = req.params;
+        const updateFields = {};
+        if(publica){
+            updateFields.publica = true;
+        }
+
+        if (name) {
+            updateFields.name = name;
+        }
+
+        if(goal){
+            updateFields.goal = goal;
+        }
+
+        try {
+            
+            const routine = await updateRoutine(id, updateFields);
+
+            if (routine) {
+                res.send({message: 'updated routine. ',
+                  data: routine
+                });
+            }else(next({
+                name: "FailToUpdateRoutineError",
+                message: 'What do you think you are trying to pull here? '
+            }))
+        
+        } catch ({name, message}) {
+            next({name, message});
+        }
+        
+
+    }
+    
+    
+
+    
 })
 
 module.exports = routinesRouter;
